e983b2af1ff60339082fe8aa21fd17e401ad99b7
VictorTarnovski <victorcruztarnovski@gmail.com>
2023-01-12 13:37:23 -0300

    feat: ensure SignUpController returns 500 if EmailValidator throws

--- src/presentation/signup.ts
+++ src/presentation/signup.ts
@@ -4,6 +4,7 @@ import { OK, BadRequest } from './helpers/http-helper'
 import { Controller } from './protocols/controller'
 import { EmailValidator } from './protocols/email-validator'
 import { InvalidParamError } from './errors/invalid-param-error'
+import { ServerError } from './errors/server-error'
 
 export class SignUpController implements Controller {
     private readonly emailValidator: EmailValidator
@@ -11,17 +12,24 @@ export class SignUpController implements Controller {
         this.emailValidator = emailValidator
     }
     handle(httpRequest: HttpRequest): HttpResponse {
-        const requiredFields = ['name','email','password','passwordConfirmation']
-        for (let field in requiredFields) {
-            const fieldValue = requiredFields[field];
-            if (!httpRequest.body[fieldValue]) {
-                return BadRequest(new MissingParamError(`${fieldValue}`))
+        try {
+            const requiredFields = ['name', 'email', 'password', 'passwordConfirmation']
+            for (let field in requiredFields) {
+                const fieldValue = requiredFields[field];
+                if (!httpRequest.body[fieldValue]) {
+                    return BadRequest(new MissingParamError(`${fieldValue}`))
+                }
+            }
+            const isValid = this.emailValidator.isValid(httpRequest.body.email)
+            if (!isValid) {
+                return BadRequest(new InvalidParamError('email'))
+            }
+            return OK()
+        } catch (error){
+            return {
+                statusCode: 500,
+                body: new ServerError
             }
         }
-        const isValid = this.emailValidator.isValid(httpRequest.body.email)
-        if (!isValid) {
-            return BadRequest(new InvalidParamError('email'))
-        }
-        return OK()
     }
 }
